c4ad12a6ad8a7a37be53224eeeedec2f83e931d3
ishland <ishlandmc@yeah.net>
2018-05-26 18:39:05 +0800

    Update

---
+++ main.php
@@ -0,0 +1,22 @@
+<?php
+
+function syntax ($src = ".")
+{
+    global $fileErrors;
+    $dir = opendir($src);
+    while (($file = readdir($dir)) !== false) {
+        if (($file != '.') && ($file != '..')) {
+            if (is_dir($src . '/' . $file)) {
+                checkFiles($src . '/' . $file);
+            } else {
+                $result = exec("php -l " . $src . '/' . $file);
+                if (strchr($result, "Errors parsing")) {
+                    $fileErrors ++;
+                    echo "[Error] Parsing error, the script will exit.\n";
+                }
+                echo $result . "\n";
+            }
+        }
+    }
+    closedir($dir);
+}

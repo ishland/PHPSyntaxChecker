156e0ff906c04ba1715657db600473651b698b69
ishland <ishlandmc@yeah.net>
2018-05-26 18:49:04 +0800

    Update

--- main.php
+++ main.php
@@ -6,14 +6,14 @@ function syntaxCheck ($src = ".")
     global $fileErrors;
     $dir = opendir($src);
     while (($file = readdir($dir)) !== false) {
-        if (($file != '.') && ($file != '..')) {
+        if (($file != '.') && ($file != '..') && (preg_match('[*.php]', $file))) {
             if (is_dir($src . '/' . $file)) {
                 checkFiles($src . '/' . $file);
             } else {
                 $result = exec("php -l " . $src . '/' . $file);
                 if (strchr($result, "Errors parsing")) {
                     $fileErrors ++;
-                    echo "[Error] Parsing error, the script will exit.\n";
+                    echo "[Error] Parsing error.\n";
                 }
                 echo $result . "\n";
             }
